0683ecf8831dac9411a454496abeeb35f5bb5ac1
Pablo Saïd <azeupaul@gmail.com>
2017-01-17 12:26:49 +0000

    Mise en place de la strucutre HTML

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php 
-
-echo 'Hello world';
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PHP revisions</title>
+		<style>
+			header{
+				background: #e3e3e3;
+				padding: 2em;
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+	<header>
+		<h1>Hello world</h1>
+	</header>
+	</body>
+</html>
