adac1dcce634f1048b0b4f02c6283fdb30e4bd58
Pablo Saïd <azeupaul@gmail.com>
2017-01-17 15:05:44 +0000

    Changement de syntaxe du foreach

--- view/index.php
+++ view/index.php
@@ -16,11 +16,9 @@
 	</header>
 	<article>
 		<ul>
-			<?php
-				foreach ($names as $name) {
-					echo "<li>$name</li>";
-				}
-			?>
+			<?php foreach ($names as $name): ?>
+				<li><?= $name; ?></li>
+			<?php endforeach; ?>
 		</ul>
 	</article>
 	</body>
