8e721c54254ae39754ad5164bd49629e94752f90
Pablo Saïd <azeupaul@gmail.com>
2017-01-19 17:58:27 +0100

    Correction de la méthode findAll

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -11,7 +11,7 @@ class QueryBuilder
 
     public function findAll($table)
     {
-        $query = $this->db_instance->prepare('SELECT * FROM {$table}');
+        $query = $this->db_instance->prepare("SELECT * FROM {$table}");
         $query->execute();
 
         return $query->fetchAll(PDO::FETCH_CLASS);
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require 'database/Connector.php';
 require 'database/QueryBuilder.php';
+require 'functions.php';
 require 'class/Task.php';
 $greetings = 'Hello world';
 
@@ -12,5 +13,12 @@ $builder = new QueryBuilder($instance);
 
 $tasks = $builder->findAll('todos');
 
+$tasks = array_map(function($task){
+    $t = new Task($task->title);
+    if($task->completed > 0){
+        $t->complete();
+    }
+    return $t;
+}, $tasks);
 
 require 'view/index.php';
