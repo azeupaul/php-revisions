bccf2c49ffecb874495c6c95659927e5df6383fe
Pablo Saïd <azeupaul@gmail.com>
2017-01-20 11:06:46 +0100

    Externalisation de la config de la base de données

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'name' => 'revisions',
+        'username' => 'root',
+        'password' => 'root',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ]
+];
--- database/Connector.php
+++ database/Connector.php
@@ -2,11 +2,16 @@
 
 class Connector
 {
-    public static function make()
+    public static function make($config)
     {
         try{
 
-            return new PDO('mysql:host=localhost;dbname=revisions', 'root', 'root');
+            return new PDO(
+                'mysql:host='.$config['host'].';dbname='.$config['name'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+            );
         }catch(PDOException $e){
             die($e->getMessage());
         }
