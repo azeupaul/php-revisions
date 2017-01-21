b4d87713c6fee28d46fc6c9bff0b95c28c082d2d
Pablo Saïd <azeupaul@gmail.com>
2017-01-21 16:48:35 +0100

    Ajout de méthode load dans le router

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ require 'class/Task.php';
 
 $builder = require 'core/bootstrap.php';
 
-$router = new Router();
-
-require 'routes.php';
+$router = Router::load('routes.php');
 
 require $router->direct(trim($_SERVER['REQUEST_URI'], '/'));
 
